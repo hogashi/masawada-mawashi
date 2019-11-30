00e2179ead6f84ed0dfba3dd7c366780853e3d32
hogashi <azuma.ginkakuji@gmail.com>
2019-12-01 04:47:07 +0900

    rename

--- master-mawashi.sh
+++ master-mawashi.sh
@@ -1,7 +1,7 @@
 for i in {1..6}; do
   if [ $i -eq 1 ]; then
-    cat master-mawashi.cs | perl -pe "s/__namei__//" | perl -pe "s/__numberi__/1/" > mawashi.cs
+    cat master-mawashi.cs.template | perl -pe "s/__namei__//" | perl -pe "s/__numberi__/1/" > mawashi.cs
   else
-    cat master-mawashi.cs | perl -pe "s/__(name|number)i__/${i}/" > mawashi${i}.cs
+    cat master-mawashi.cs.template | perl -pe "s/__(name|number)i__/${i}/" > mawashi${i}.cs
   fi
 done
